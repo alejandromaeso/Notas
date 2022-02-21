6e15074323ad1df89ba1bb74cdcc5a3f07994f82
Alejandrico <Alejandrico@DESKTOP-DUKH0VE>
2022-02-21 20:14:52 +0100

    5.	Utiliza las funciones de refactorización para cambiar la cabecera del método "cali". Le cambiarás el nombre por "califica" y, en el mismo paso, cambiarás el nombre del parámetro por "num_nota". El método anterior tendrá que quedar en desuso. (En Eclipse y en Netbeans). Sube las modificaciones al repositorio de Github, siendo el comentario el enunciado de este ejercicio.

--- Tarea04_ENDES/src/notas/Notas.java
+++ Tarea04_ENDES/src/notas/Notas.java
@@ -15,22 +15,30 @@ public class Notas {
 		System.out.print("Introduzca una nota: ");
 		nota = reader.nextInt();
 
-		cali(nota);
+		califica(nota);
 		reader.close();
 	}
 
 	/**
 	 * @param nota
+	 * @deprecated Use {@link #califica(int)} instead
 	 */
 	private static void cali(int nota) {
+		califica(nota);
+	}
+
+	/**
+	 * @param num_nota
+	 */
+	public static void califica(int num_nota) {
 		String calificacion;
-		if (nota >= 0 && nota < 5)
+		if (num_nota >= 0 && num_nota < 5)
 			calificacion = "Suspenso";
-		else if (nota >= 5 && nota < 7)
+		else if (num_nota >= 5 && num_nota < 7)
 			calificacion = "Aprobado";
-		else if (nota >= 7 && nota < 9)
+		else if (num_nota >= 7 && num_nota < 9)
 			calificacion = "Notable";
-		else if (nota >= 9 && nota <= 10)
+		else if (num_nota >= 9 && num_nota <= 10)
 			calificacion = "Sobresaliente";
 		else
 			calificacion = "La nota introducida no es correcta";
